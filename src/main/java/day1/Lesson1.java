b8d8c5fdd7e9f2971f1d9ef1cc15450bdb22be94
YaroslavKolomytsev <zlo200883@gmail.com>
2023-02-22 22:32:31 +0300

    lesson one update

--- src/main/java/day1/Lesson1.java
+++ src/main/java/day1/Lesson1.java
@@ -5,10 +5,10 @@ public class Lesson1 {
         /*1. Вывести на экран слово “JAVA”, в строку, чтобы оно повторилось 10 раз, используя цикл while.
 Вывод в консоль должен быть таким:
 JAVA JAVA JAVA JAVA JAVA JAVA JAVA JAVA JAVA JAVA */
-        int i = 10;
-        while(i > 0){
+        int i = 0;
+        while(i <= 9){
             System.out.print("JAVA ");
-            i--;
+            i++;
         }
     }
 }
